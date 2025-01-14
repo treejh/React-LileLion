2d06ea1f4e0d656a093bc99c0630c9ffe52385e7
jihyun <jihyun3066@naver.com>
2025-01-14 14:50:13 +0900

    [refact] todoList 수정 추가

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import Todo from "./Todo";
 
-const TodoList=({todoList, deleteHandler})=>{
+const TodoList=({todoList, deleteHandler, updateHandler })=>{
 
     return(
     
     <ul>
        { todoList.map((todo)=>(
                 <li key={todo.id}>
-                    <Todo todo={todo} deleteHandler={deleteHandler}/></li>
+                    <Todo todo={todo} deleteHandler={deleteHandler} updateHandler={updateHandler}/></li>
             
        ))
         }
